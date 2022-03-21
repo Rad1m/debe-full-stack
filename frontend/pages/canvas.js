7af0a05f601203c160b98b1ab3182986e342c806
Rad1m <radim.simanek@protonmail.ch>
2022-03-21 11:37:15 +0000

    for loop renders grid

--- frontend/pages/canvas.js
+++ frontend/pages/canvas.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Games } from "./games";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { gameInfo } from "./atoms/atoms";
 import styles from "../styles/Home.module.css";
 
 export default function Canvas() {
-  return (
-    <div className={styles.grid}>
-      <Games id={0} />
-    </div>
-  );
+  let gameList = [];
+  var i = 0;
+  while (i < 2) {
+    gameList.push(<Games key={i} id={i} />);
+    const gameInf = useRecoilValue(gameInfo(i));
+    console.log(gameInf.gameName);
+    i++;
+  }
+
+  return <div className={styles.grid}>{gameList}</div>;
 }
